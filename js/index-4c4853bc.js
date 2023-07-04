73ba7317b2fb1a295c2162ae8e81b0d52449c852
TING <1127163160@qq.com>
2023-07-04 20:23:56 +0800

    page

---
+++ js/index-4c4853bc.js
@@ -0,0 +1,1 @@
+import{_ as o,aC as r,o as n,j as t}from"./index-96b2ba3a.js";const c={};function s(_,a){const e=r("router-view");return n(),t(e)}const i=o(c,[["render",s]]);export{i as default};
---
+++ js/zh-CN-15bc1800.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-96b2ba3a.js";const t={settings:{title:"系统设置"}},o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),s=Object.assign({"./zh-CN/layout.ts":o}),a={message:{...e(s,"zh-CN")}};export{a as default};
